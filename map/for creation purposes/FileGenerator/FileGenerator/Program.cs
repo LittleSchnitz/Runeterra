dba7f3bb9f4c421e38f81470f91154a9b6ac38c0
Kasper Knigge Trantel <kaspertrantel@gmail.com>
2020-01-30 18:09:37 +0100

    FileGenerator

---
+++ map/for creation purposes/FileGenerator/FileGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileGenerator
+{
+    class Program
+    {
+        public static string[] lines = 
+        {
+            "#", "", "add_core = ", "own = ", "controller = ", "culture = ", "religion = ",
+            "hre = no", "base_tax = 1", "base_production = 1", "base_manpower = 1", "trade_goods = ",
+            "is_city = yes", "discorvered_by = "
+        };
+
+        public static void CreateEmptyFile(string filename)
+        {
+            //File.Create(filename+".txt").Dispose();
+            File.WriteAllLines(filename + ".txt", lines);
+        }
+
+        public static void RenameToFirstLine(int num)
+        {
+            if (File.Exists(num + " - "+".txt") && new FileInfo(num + " - "+".txt").Length != 0)
+            {
+                string name = File.ReadLines(num + " - "+".txt").First();
+                name = name.Substring(1);
+                File.Move(num + " - " + ".txt", num + " - " + name+".txt");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                CreateEmptyFile(i+" - ");
+                //RenameToFirstLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
